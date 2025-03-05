cba31043e8092bf0f1e775d46cc1eb8e9f35be51
ashkanrabiee <ashkanrabiee52@gmail.com>
2025-03-05 02:32:41 -0800

    add route and controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Admin\Market\BrandController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\Market\CategoryController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,6 +21,29 @@ Route::middleware([
 
 
 
+Route::prefix('admin')->group(function(){
+    Route::get('/', [AdminDashboardController::class, 'index'])->name('admin.home');
+
+    Route::prefix('market')->group(function(){
+
+        // category routes
+        Route::prefix('category')->group(function () {
+            Route::get('/', [CategoryController::class, 'index'])->name('admin.market.category.index');
+            Route::get('/create', [CategoryController::class, 'create'])->name('admin.market.category.create');
+            Route::post('/store', [CategoryController::class, 'store'])->name('admin.market.category.store');
+            Route::get('/edit/{productCategory}', [CategoryController::class, 'edit'])->name('admin.market.category.edit');
+            Route::put('/update/{productCategory}', [CategoryController::class, 'update'])->name('admin.market.category.update');
+            Route::delete('/destroy/{productCategory}', [CategoryController::class, 'destroy'])->name('admin.market.category.destroy');
+        });
+        //brand
+        Route::prefix('brand')->group(function(){
+            Route::get('/' , [BrandController::class , 'index'])->name('admin.market.brand.index');
+
+
+        });
+
+    });
+});
 
 
 
