a083995269e0a03ba0c52aeadc2435ad322916a0
ashkanrabiee <ashkanrabiee52@gmail.com>
2025-03-06 06:23:40 -0800

    create menu creator in admin panel

--- app/Http/Controllers/Admin/Content/MenuController.php
+++ app/Http/Controllers/Admin/Content/MenuController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Admin\Content;
 
-use App\Http\Controllers\Controller;
+use App\Models\Content\Menu;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Content\MenuRequest;
 
 class MenuController extends Controller
 {
@@ -12,7 +14,9 @@ class MenuController extends Controller
      */
     public function index()
     {
-        //
+        $menus = Menu::orderBy('created_at', 'desc')->simplePaginate(15);
+        // dd($menus[2]->children);
+        return view('admin.content.menu.index', compact('menus'));
     }
 
     /**
@@ -20,15 +24,18 @@ class MenuController extends Controller
      */
     public function create()
     {
-        //
+        $menus = Menu::where('parent_id', null)->get();
+        return view('admin.content.menu.create', compact('menus'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(MenuRequest $request)
     {
-        //
+        $inputs = $request->all();
+        $menu = Menu::create($inputs);
+        return redirect()->route('admin.content.menu.index')->with('swal-success', 'منوی  جدید شما با موفقیت ثبت شد');
     }
 
     /**
@@ -42,24 +49,47 @@ class MenuController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Menu $menu)
     {
-        //
+        $parent_menus = Menu::where('parent_id', null)->get()->except($menu->id);
+        return view('admin.content.menu.edit', compact('menu' ,'parent_menus'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(MenuRequest $request, Menu $menu)
     {
-        //
+        $inputs = $request->all();
+        $menu->update($inputs);
+        return redirect()->route('admin.content.menu.index')->with('swal-success', 'منوی  شما با موفقیت ویرایش شد');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Menu $menu)
     {
-        //
+        $result = $menu->delete();
+        return redirect()->route('admin.content.menu.index')->with('swal-success', ' منو شما با موفقیت حذف شد');
+    }
+
+
+    public function status(Menu $menu){
+
+        $menu->status = $menu->status == 0 ? 1 : 0;
+        $result = $menu->save();
+        if($result){
+                if($menu->status == 0){
+                    return response()->json(['status' => true, 'checked' => false]);
+                }
+                else{
+                    return response()->json(['status' => true, 'checked' => true]);
+                }
+        }
+        else{
+            return response()->json(['status' => false]);
+        }
+
     }
 }
